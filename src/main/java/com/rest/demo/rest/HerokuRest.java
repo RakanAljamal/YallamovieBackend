30d5217ffebd11f5d2887676650b7eadff29e97d
Rakan <rakanaljamal@hotmail.com>
2019-11-20 08:47:58 +0200

    'bekaffi'

--- src/main/java/com/rest/demo/rest/HerokuRest.java
+++ src/main/java/com/rest/demo/rest/HerokuRest.java
@@ -1,14 +1,36 @@
 package com.rest.demo.rest;
 
+import com.rest.demo.entity.Movie;
+import com.rest.demo.service.MovieService;
+import com.rest.demo.service.MyMovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class HerokuRest {
-@RequestMapping("/")
+    @Autowired
+    @Qualifier("MovieService")
+    private MovieService movieService;
+    @Autowired
+    @Qualifier("MyMovieService")
+    private MyMovieService myMovieService;
+
+    @RequestMapping("/")
     public String showHome(){
     return "Hello-heroku";
 }
+
+    @GetMapping("/movie")
+    private List<Movie> showMovies() {
+        return movieService.findAllMovies();
+    }
+
+
 }
