dc0ceed6d0c25b97e81dffbba5b7851470b306bf
Rakan <rakanaljamal@hotmail.com>
2019-11-20 08:44:57 +0200

    'please'

--- src/main/java/com/rest/demo/rest/DemoRest.java
+++ src/main/java/com/rest/demo/rest/DemoRest.java
@@ -1,70 +1,70 @@
-//package com.rest.demo.rest;
-//
-//import com.rest.demo.entity.Movie;
-//import com.rest.demo.service.MovieService;
-//import com.rest.demo.service.MyMovieService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api")
-//@CrossOrigin
-//public class DemoRest {
+package com.rest.demo.rest;
+
+import com.rest.demo.entity.Movie;
+import com.rest.demo.service.MovieService;
+import com.rest.demo.service.MyMovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class DemoRest {
+    @Autowired
+    @Qualifier("MovieService")
+    private MovieService movieService;
+    @Autowired
+    @Qualifier("MyMovieService")
+    private MyMovieService myMovieService;
+
 //    @Autowired
-//    @Qualifier("MovieService")
-//    private MovieService movieService;
-//    @Autowired
-//    @Qualifier("MyMovieService")
-//    private MyMovieService myMovieService;
-//
-////    @Autowired
-////    @Qualifier("myOwnMovieService")
-////    private MyOwnMovieService myOwnMovieService;
-//
-//    @GetMapping("/movie")
-//    private List<Movie> showMovies() {
-//        return movieService.findAllMovies();
-//    }
-//
-//
-//    //  @GetMapping("/smovie/{movieName}?limit={limit}")
-//    @GetMapping("/smovie/{movieName}")
-//    private List<Movie> showName(@PathVariable String movieName, @RequestParam(value = "limit", defaultValue = "15") int limit) {
-//        List<Movie> movies;
-//        try {
-//            movies = myMovieService.findMovieByName(movieName);
-//            movies = limit > movies.size() ? movies : movies.subList(0, limit);
-//        } catch (IndexOutOfBoundsException e) {
-//            movies = new ArrayList<>();
-//        }
-//        return movies;
-//    }
-//
-//
-//    @GetMapping("/movie/{movieId}")
-//    private Movie showMovie(@PathVariable int movieId) {
-//        return movieService.findMovieById(movieId);
-//    }
-//
-//    @PostMapping("/movie")
-//    private void saveMovie(@RequestBody Movie movie) {
-//        movieService.saveMovie(movie);
-//    }
-//
-//    @PutMapping("/movie")
-//    private String editMovie(@RequestBody Movie movie) {
-//        movieService.saveMovie(movie);
-//        return "UPDATED";
-//    }
-//
-//
-//    @DeleteMapping("/movie/{movieId}")
-//    private String deleteTheMovie(@PathVariable int movieId) {
-//        movieService.deleteMovie(movieId);
-//        return "Deleted Successfully";
-//    }
-//}
+//    @Qualifier("myOwnMovieService")
+//    private MyOwnMovieService myOwnMovieService;
+
+    @GetMapping("/movie")
+    private List<Movie> showMovies() {
+        return movieService.findAllMovies();
+    }
+
+
+    //  @GetMapping("/smovie/{movieName}?limit={limit}")
+    @GetMapping("/smovie/{movieName}")
+    private List<Movie> showName(@PathVariable String movieName, @RequestParam(value = "limit", defaultValue = "15") int limit) {
+        List<Movie> movies;
+        try {
+            movies = myMovieService.findMovieByName(movieName);
+            movies = limit > movies.size() ? movies : movies.subList(0, limit);
+        } catch (IndexOutOfBoundsException e) {
+            movies = new ArrayList<>();
+        }
+        return movies;
+    }
+
+
+    @GetMapping("/movie/{movieId}")
+    private Movie showMovie(@PathVariable int movieId) {
+        return movieService.findMovieById(movieId);
+    }
+
+    @PostMapping("/movie")
+    private void saveMovie(@RequestBody Movie movie) {
+        movieService.saveMovie(movie);
+    }
+
+    @PutMapping("/movie")
+    private String editMovie(@RequestBody Movie movie) {
+        movieService.saveMovie(movie);
+        return "UPDATED";
+    }
+
+
+    @DeleteMapping("/movie/{movieId}")
+    private String deleteTheMovie(@PathVariable int movieId) {
+        movieService.deleteMovie(movieId);
+        return "Deleted Successfully";
+    }
+}
