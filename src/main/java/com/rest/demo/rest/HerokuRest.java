d520f9b1afb05766f9d4bfc24f9b254f5ef53982
Rakan <rakanaljamal@hotmail.com>
2019-11-20 08:26:49 +0200

    'new'

--- src/main/java/com/rest/demo/rest/HerokuRest.java
+++ src/main/java/com/rest/demo/rest/HerokuRest.java
@@ -1,9 +1,11 @@
 package com.rest.demo.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class HerokuRest {
 @RequestMapping("/")
     public String showHome(){
